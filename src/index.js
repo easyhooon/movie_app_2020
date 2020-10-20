2527b8c2f56effbf79a1b41632ab72d36189fb09
easyhooon <51016231+easyhooon@users.noreply.github.com>
2020-10-20 17:37:56 +0900

    change and run well

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import './index.css';
 import App from "./App";
-//import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App /><Potato />
-//   </React.StrictMode>,
-//   document.getElementById("potato")
-// );
 
 ReactDOM.render(<App />, document.getElementById("potato"));
 
@@ -17,10 +9,6 @@ ReactDOM.render(<App />, document.getElementById("potato"));
 //따라서 모든 것은 application 안에 들어가야만 함
 //application 안에서 많은 component를 넣을 수 있어, 그리고 이러한 component안에 더 많은 component를 import할 수 있어
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
 
 //component는 HTML을 반환하는 함수
 //<App /> -> 이 부분이 우리가 component를 사용하고자 할 때, component의 형태
---
+++ src/routes/Detail.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
+}
+export default Detail;
+
+//실행 순서 render() - > componentDidMount
+
+// function Detail(location){
+//     console.log(location);
+//     return <span>hello</span>;
+// }
+
